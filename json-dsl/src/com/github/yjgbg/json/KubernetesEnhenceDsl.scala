e464d256d658211614f8f2f00de2887516913fe0
weicl <chenglong.wei@mail.hypers.com>
2022-10-10 20:16:34 +0800

    添加了httpServer和staticFile方法，补充了一些注释

--- json-dsl/src/com/github/yjgbg/json/KubernetesEnhenceDsl.scala
+++ json-dsl/src/com/github/yjgbg/json/KubernetesEnhenceDsl.scala
@@ -2,8 +2,23 @@ package com.github.yjgbg.json
 
 trait KubernetesEnhenceDsl:
   self: KubernetesDsl =>
-  // 会创建一个configmap用于存放脚本文件，一个cronJob会挂载configmap用于执行任务
-  // 工作目录下的.cache目录是可以缓存的，缓存的键为该cronJob
+  /**
+    * 基于ammonite和cronjob的cronJob资源
+    * 会创建一个configmap用于存放脚本文件，一个cronJob会挂载configmap用于执行任务
+    * 工作目录下的.cache目录是可以缓存的，缓存的键为该cronJob
+    * 有着自带的系统缓存，不会重复下载ammonite二进制文件，也不会反复在互联网拉取依赖包
+    *
+    * @param name 名称
+    * @param script 脚本内容
+    * @param schedule 执行周期
+    * @param suspend 是否停止
+    * @param image 镜像名，默认为jdk17，取决于你的脚本期待的运行环境
+    * @param scalaVersion scala版本，默认3.2，取决于你的脚本期待的运行环境
+    * @param ammVersion ammonite版本，默认2.5.4，取决于你的脚本期待的运行环境
+    * @param successfulJobsHistoryLimit 保存的成功job数量，默认3
+    * @param failedJobsHistoryLimit 保存的失败job数量，默认5
+    * @param env 环境变量
+    */
   def ammoniteCronJob(using Prefix, Interceptor)(
       name: String, // 名字
       script: String, // 脚本内容
@@ -74,7 +89,13 @@ trait KubernetesEnhenceDsl:
       }
     }
   }
-  // 端口映射
+  /**
+    * 端口映射服务，会创建一个单独的pod，用于映射端口，用于开发时在本地访问不到环境中的各种基础服务时使用
+    *
+    * @param name pod名称
+    * @param ip 要映射的目标的ip
+    * @param local2Remote 端口号与目标服务端口号映射关系
+    */
   def portForward(using Prefix, Interceptor)(name:String,ip:String,local2Remote:(Int,Int)*): Unit = 
       pod(name) {
         spec {
@@ -85,64 +106,56 @@ trait KubernetesEnhenceDsl:
               "REMOTE_PORT" -> remotePort.toString(),
               "LOCAL_PORT" -> localPort.toString()
               )
-          }
+            }
           })
         }
       }
 
-  // 这是一个简单的web服务器，用于做静态服务，考虑给nginxConf一个默认值
-  def nginxStaticHttpApplication(using Prefix, Interceptor)(
-      name: String,
-      image: String,
-      dirPath: String, // 静态文件存储的目录，应该以斜杠结尾，会被复制到runtimeImage的同名目录中
-      nginxConf: String, // nginx 配置文件，可以使用环境变量，会经过envsubst处理
-      runtimeImage: String = "nginx:latest", // 运行时镜像
-      replicas: Int = 1,
-      tcpPort: Int = 80,
-      initScript: Seq[String] = Seq(), // 脚本，会在启动时按顺序执行
-      env: (String, String)* // 环境变量
+  /**
+    * 简单http服务器，可以将指定镜像中的制定目录下的文件以http服务暴露出去
+    *
+    * @param name 资源名称
+    * @param image 镜像，所要暴露的文件所在的镜像
+    * @param dirPath 目录，所要暴露的文件目录在镜像中的path
+    * @param runtimeImage 运行时镜像，这个是nginx，不建议更改，
+    * 除非你所在的网络访问不到dockerhub无法拉取nginx镜像，可以改为你的内网docker registry以拉取镜像
+    * @param init 初始化脚本，支持写一系列初始化脚本，会在容器启动的时候执行
+    */
+  def simpleStaticFileHttpServer(using Prefix,Interceptor)(
+    name:String,
+    image:String,
+    dirPath:String, // 需要以斜杠结尾
+    runtimeImage:String = "nginx:latest",
+    init:String*
   ): Unit = {
-    val resourceName = s"$name-nginx-static-http-application"
-    val initScriptNameAndContent = initScript.zipWithIndex.map((script, index) => s"$index.scripts.sh" -> script)
-    // 声明configmap
-    val nginxConfTemplate = "nginx.conf.template"
+    val resourceName = s"$name-static-file-http-server"
     val labels = "app" -> resourceName
-    configMap(resourceName) {
-      data(nginxConfTemplate -> nginxConf)
-      initScriptNameAndContent.foreach(data(_))
+    if (!init.isEmpty) {
+      configMap(resourceName) {
+        for ((script,index) <- init.zipWithIndex)
+          data(s"${index}.script.sh" -> script)
+      }
     }
-    // 创建deployment
     deployment(resourceName) {
-      spec {
-        self.replicas(replicas)
+      spec{
+        replicas(1)
         selectorMatchLabels(labels)
-        template {
+        template{
           self.labels(labels)
           spec {
             val www = "www"
-            // 声明一个空目录，用来存放静态资源文件
             volumeEmptyDir(www)
-            // 声明一个存储nginxconfig的volume
-            volumeConfigMap(nginxConfTemplate, resourceName, nginxConfTemplate -> nginxConfTemplate)
-            if (!initScriptNameAndContent.isEmpty) {
-              volumeConfigMap(
-                "init-scripts",
-                resourceName,
-                initScriptNameAndContent.map((name, _) => name -> name): _*
-              )
+            if(!init.isEmpty) {
+              volumeConfigMap("script" ,resourceName)
             }
-            initContainer("prepare-static-file", image) {
-              env.foreach((k, v) => self.env(k -> v))
-              command("cp", dirPath, s"/$www/")
+            initContainer("prepare-file",image) {
               volumeMounts(www -> s"/$www/")
+              command("cp", dirPath, s"/$www/")
             }
-            container(resourceName, runtimeImage) {
-              volumeMounts(www -> dirPath)
-              volumeMounts(nginxConfTemplate -> "/etc/nginx/templates/")
-              env.foreach((k, v) => self.env(k -> v))
-              // 如果存在初始化脚本,则声明一个用来存储初始化脚本的卷,并挂载初始化脚本
-              if (!initScriptNameAndContent.isEmpty) {
-                volumeMounts("init-scripts" -> "/docker-entrypoint.d/40-custom/")
+            container("app","nginx:latest") {
+              volumeMounts(www -> "/usr/share/nginx/html")
+              if (!init.isEmpty) {
+                volumeMounts("script" -> "/docker-entrypoint.d/40-custom/")
               }
             }
           }
@@ -152,11 +165,89 @@ trait KubernetesEnhenceDsl:
     service(resourceName) {
       spec {
         selectorMatchLabels(labels)
-        tcpPorts(tcpPort -> tcpPort)
+        tcpPorts(80 -> 80)
+      } 
+    }
+  }
+
+  /**
+    * 
+    *
+    * @param name 名字
+    * @param conf 配置文件内容
+    * @param image 镜像tag
+    * @param ports 端口号
+    */
+  def simpleNginxServer(using Prefix, Interceptor)(
+    name:String,
+    conf:String,
+    image:String = "nginx:latest",
+    ports:Seq[Int] = Seq(80)
+  ) = nginxServer(name,Map("nginx.conf" -> conf),image,ports,Map())
+
+  /**
+    * 这个主要用途是做代理,一般而言，配置name以及一个conf文件即可
+    *
+    * @param name
+    * @param conf 配置文件名和内容的映射，会添加.template后缀，然后被放到/etc/nginx/templates/目录下，
+    * 再经由nginx启动时候的envsubst渲染环境变量，到/etc/nginx/conf.d目录，可以用美元符号使用环境变量
+    * @param image
+    * @param ports
+    * @param env
+    */
+  def nginxServer(using Prefix, Interceptor)(
+    name:String,
+    conf:Map[String,String] = Map(), // 配置文件名和内容，会被放到/etc/nginx/templates/目录下，可以使用环境变量
+    image:String = "nginx:latest",
+    ports:Seq[Int] = Seq(80),
+    env:Map[String,String] = Map()
+  ) : Unit = {
+    val resourceName = s"$name-nginx-gateway"
+    if(!conf.isEmpty) {
+      configMap(resourceName) {
+        conf.foreach((k,v) => data(s"${k}.template" -> v))
+      }
+    }
+    val labels = "app" -> resourceName
+    deployment(resourceName) {
+      spec {
+        selectorMatchLabels(labels)
+        template{
+          self.labels(labels)
+          spec {
+            if (!conf.isEmpty) {
+              volumeConfigMap("config",resourceName)
+              container("container",image) {
+                volumeMounts("config" -> "/etc/nginx/templates/")
+              }
+            } else {
+              container("container",image) {}
+            }
+          }
+        }
+      }
+    }
+    service(resourceName) {
+      spec {
+        selectorMatchLabels(labels)
+        ports.foreach(it => tcpPorts(it -> it))
       }
     }
   }
 
+  /**
+    * nacos中的一条配置文件
+    * 会创建一个ammoniteCronJob资源，周期性地将数据复写到nacos中，防止被更改
+    *
+    * @param host nacos访问地址
+    * @param username nacos用户名
+    * @param password nacos密码
+    * @param namespace nacos中的命名空间 
+    * @param group nacos中的group
+    * @param name nacos中的dataId
+    * @param value 文件的值
+    * @param schedule 同步时间
+    */
   def nacosConfig(using Prefix, Interceptor)(
       host: String,
       username: String,
