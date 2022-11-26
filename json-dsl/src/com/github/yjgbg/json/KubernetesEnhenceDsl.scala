d873de61e0999402cd2cf784b0dc8dee86eb3eab
weicl <chenglong.wei@mail.hypers.com>
2022-11-26 17:56:36 +0800

    添加了ammonite方法

--- json-dsl/src/com/github/yjgbg/json/KubernetesEnhenceDsl.scala
+++ json-dsl/src/com/github/yjgbg/json/KubernetesEnhenceDsl.scala
@@ -6,7 +6,7 @@ trait KubernetesEnhenceDsl:
   given UtilsImage = "alpine:latest"
   def utilsImage(image:String):UtilsImage = image
   def simplePVC(using interceptor:Interceptor,prefix:Prefix)
-  (name:String,size:Long = 5,storageClass:String = null) = // 创建一个pvc
+  (name:String,size:Long = 5,storageClass:String|Null = null) = // 创建一个pvc
     persistentVolumeClaim(name) {
       spec {
         accessModes("ReadWriteOnce")
@@ -23,8 +23,8 @@ trait KubernetesEnhenceDsl:
     path:String):Unit = { 
       volumeEmptyDir(name)
       initContainer(name,image) {
-        volumeMounts(name -> s"/tmp/$name")
-        command("sh","-c",s"cp -r $path /tmp/$name")
+        volumeMounts(name -> s"/tmp/vol")
+        command("sh","-c",s"cp -r $path /tmp/vol")
       }
     }
   def proxy(using PodScope >> SpecScope)(
@@ -34,7 +34,7 @@ trait KubernetesEnhenceDsl:
     image:String = "marcnuri/port-forward"
   )(closure: PodScope >> SpecScope >> ConfigableScope ?=> Unit) = {
     val localPort0 = (if localPort != null then localPort else port).toString()
-    container(localPort0,image) {
+    container(s"proxy-$localPort0",image) {
       env(
         "REMOTE_HOST" -> ip,
         "REMOTE_PORT" -> port.toString(),
@@ -55,5 +55,52 @@ trait KubernetesEnhenceDsl:
       val cmds = for ((k,v) <- indexAndKeyAndValues) yield 
         s"""echo "${"$"}{${k}}" > /literial/${v._1}"""
       command("sh","-c",cmds.mkString("\n"))
+    }
+  }
+
+  def prepareAmmonite(using Prefix,Interceptor)(ammoniteSize:Long = 5,coursierSize:Long = 5,storageClass:String|Null):Unit = {
+    simplePVC("ammonite-cache",ammoniteSize,storageClass)
+    simplePVC("coursier-cache",coursierSize,storageClass)
+  }
+  def ammonite(using PodScope >> SpecScope,UtilsImage)
+  (
+    name:String,
+    script:String,
+    ammVersion:String = "2.5.5",
+    scalaVersion:String = "3.2",
+    image:String = "eclipse-temurin:17-jdk"
+  )(closure : PodScope >> SpecScope >> ContainerScope ?=> Unit ) : Unit = {
+    val ammDownloadPath = "/root/.ammonite/download"
+    val ammExecPath = s"$ammDownloadPath/${ammVersion}_$scalaVersion"
+    val downloadFile = s"$ammExecPath-tmp-download"
+    val ammDownloadUrl =
+      s"https://github.com/lihaoyi/ammonite/releases/download/${ammVersion.split("-")(0)}/$scalaVersion-$ammVersion"
+    volumeFromLiterialText("scripts",
+      //这个amm文件是一个wrapper 脚本，会自动从github release pages 下载指定版本的ammonite
+      "amm" -> s"""
+        |#!/usr/bin/env sh
+        |set -e
+        |if [ ! -x "$ammExecPath" ] ; then
+        |  mkdir -p $ammDownloadPath
+        |  curl --fail -L -o "$downloadFile" "$ammDownloadUrl"
+        |  chmod +x "$downloadFile"
+        |  mv "$downloadFile" "$ammExecPath"
+        |fi
+        |exec ${ammExecPath} "${"$"}@"
+        |""".stripMargin.stripLeading().stripTrailing(),
+      "script.sc" ->  script
+    )
+    volumePVC("coursier-cache")
+    volumePVC("ammonite-cache")
+    container(name,image) {
+      volumeMounts("scripts" -> "/workspace")
+      volumeMounts("coursier-cache" -> "/coursiercache")
+      volumeMounts("ammonite-cache" -> "/root/.ammonite/download")
+      env("COURSIER_CACHE" -> "/coursiercache")
+      command("sh","-c",s"""
+        |chmod -R a+x /workspace/amm
+        |/workspace/amm /workspace/script.sc
+        |""".stripMargin.stripLeading().stripTrailing())
+      closure.apply
     }
   }
